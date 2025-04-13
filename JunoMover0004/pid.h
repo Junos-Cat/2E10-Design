c3228a1755f44ac0bfcde65d52d1fc9144f07b83
Junos-Cat <keaneyls@tcd.ie>
2025-04-13 13:58:21 +0100

    Universal implementation of pid control

--- JunoMover0004/pid.h
+++ JunoMover0004/pid.h
@@ -4,37 +4,38 @@
 #include "motorControl.h"
 
 void pid(){
+
+  // Error
   if (UIMode == 1){ // Speed control
-    // Error for speed
     E = RPMDesired - (leftRPM + rightRPM)/2;
-
-    // Integration
-    Inte = IntePrevious + (dt * (E + EPrevious) * half);
-
-    // Differenciation
-    Diff = (E - EPrevious) / dt;
-
-    // Voltage
-    V = VPrevious + (kpSpeed * E + kiSpeed * Inte + kdSpeed * Diff);
-
-
-    // Prevention of antiwinder
-    if (V > Vmax) {
-      V = Vmax;
-      Inte = IntePrevious;
-    }
-    if (V < Vmin) {
-      V = Vmin;
-      Inte = IntePrevious;
-    }
-
-    // Update voltages
-    VPrevious = V;
-
-    left_motor_move(V * leftF, Vmax);
-    right_motor_move(V * rightF, Vmax);
   }
   else if (UIMode == 2){ // Distance control
+    E = USSensorDistance - DistanceDesired;
   }
-  
+
+  // Integration
+  Inte = IntePrevious + (dt * (E + EPrevious) * half);
+
+  // Differenciation
+  Diff = (E - EPrevious) / dt;
+
+  // Voltage
+  V = VPrevious + (kpSpeed * E + kiSpeed * Inte + kdSpeed * Diff);
+
+
+  // Prevention of antiwinder
+  if (V > Vmax) {
+    V = Vmax;
+    Inte = IntePrevious;
+  }
+  if (V < Vmin) {
+    V = Vmin;
+    Inte = IntePrevious;
+  }
+
+  // Update voltages
+  VPrevious = V;
+
+  left_motor_move(V * leftF, Vmax);
+  right_motor_move(V * rightF, Vmax);
 }
